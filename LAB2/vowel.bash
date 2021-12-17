9da00bac855b2de9f3845bf4de98cc103c043a96
Deepthi-10 <72984673+Deepthi-10@users.noreply.github.com>
2021-12-17 09:51:40 +0530

    Create vowel.bash

---
+++ LAB2/vowel.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "enter the string to count vowels"
+read str
+len=`expr length $str`
+count=0
+while [ $len -gt 0 ]
+do
+ch=`echo $str | cut -c $len`
+case $ch in 
+  [aeiouAEIOU] )
+  count=`expr $count + 1`
+  echo $ch
+  ;;
+  esac
+  len=`expr $len - 1`
+  done
+echo "the number of vowels are" $count
